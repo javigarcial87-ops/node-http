ac5d3bea03647bfbfb015c0ffff657f0c9324d1d
javier garcia alvarez <javigarcial87@gmail.com>
2025-12-15 23:51:30 +0100

    ejercicio backend 1

---
+++ data.js
@@ -0,0 +1,9 @@
+const webInfo = {  //creamos un objeto con la base de datos
+    title: "miprimera web con node",
+    subtitle:"Aprendiendo paso a paso",
+    description: "Esta pagina se genera desde un server HTTP en node.js"
+
+}
+module.exports= webInfo; //esto permite que otro archivo lo use
+
+//aqui no hay servidor, solo informacion
---
+++ server.js
@@ -0,0 +1,4 @@
+const http =require('node:http'); //esto nos da las herramientas para crear un servidor por defecto
+const webInfo = require('./data.js'); // con esto le decimos que queremos usar los datos del otro archivo (./ significa que esta dentro de esta misma carpeta)
+
+const PORT = 3000 //
